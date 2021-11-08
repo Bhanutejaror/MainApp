ae902231bf5a5fc3cfa9131eaef60795fcdb3786
bhanuteja <bhanutejaror@gmail.com>
2021-11-08 17:42:32 +0530

    main app

---
+++ db/migrate/20160615171538_create_reservations.rb
@@ -0,0 +1,26 @@
+class CreateReservations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reservations do |t|
+
+      t.string :name
+      t.string :phone_number
+      t.string :cake_size
+      t.string :flavour
+      t.string :delivery_or_pickup
+      t.string :delivery_address
+      t.string :d_phone_number
+      t.string :amount
+      t.string :paid
+      t.string :balance
+      t.string :date
+      t.string :time
+      t.string :image
+
+      t.timestamps null: false
+
+      t.timestamps null: false
+
+      
+    end
+  end
+end
