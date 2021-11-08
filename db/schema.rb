e57ac6a84a1ed118cd9056d8461b26185d2c9a55
bhanutejaror <bhanutejaror.com>
2021-11-08 21:49:54 +0530

    sadasD

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2016_06_15_171538) do
 
-   create_table "articles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "reservations", force: :cascade do |t|
     t.string "name"
     t.string "phone_number"
